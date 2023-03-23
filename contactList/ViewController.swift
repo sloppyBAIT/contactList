66a448b7df7302215c7a737ff34f76d56e37cbe4
Stogov Anton <sloppybait@yandex.ru>
2023-03-23 14:18:52 +0300

    Initial Commit

---
+++ contactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contactList
+//
+//  Created by Anton Stogov on 23/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
